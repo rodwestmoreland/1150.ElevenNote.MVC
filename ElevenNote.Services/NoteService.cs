cab04cba14ef57ba03776807743dbcb5eae72bf0
Rod <48662639+rodwestmoreland@users.noreply.github.com>
2021-03-24 22:25:16 -0400

    init commit

---
+++ ElevenNote.Services/NoteService.cs
@@ -0,0 +1,71 @@
+﻿using ElevenNote.Data;
+using ElevenNote.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElevenNote.Services
+{
+    public class NoteService
+    {
+        private readonly Guid _userId;
+        private readonly ApplicationDbContext _context = new ApplicationDbContext();
+        public NoteService(Guid userId) => _userId = userId;
+        
+
+        public bool CreateNote(NoteCreate model)
+        {
+            var entity = new Note()
+            {
+                OwnerId =   _userId,
+                Title =     model.Title,
+                Content =   model.Content,
+                CreatedUtc = DateTimeOffset.Now
+            };
+
+            _context.Notes.Add(entity);
+            return _context.SaveChanges() == 1;
+        }
+
+
+
+        public IEnumerable<NoteListItem> GetNotes()
+        {
+            var query = _context.Notes.Where(x => x.OwnerId == _userId)
+                                        .Select(x => new NoteListItem
+                                        {
+                                            NoteId = x.NoteId,
+                                            Title = x.Title,
+                                            CreatedUtc = x.CreatedUtc
+                                        });
+            return query.ToArray();
+        }
+
+        public NoteDetail GetNoteById(int id)
+        {
+            var entity = _context.Notes.Single(x => x.NoteId == id &&
+                                                    x.OwnerId == _userId);
+            return new NoteDetail
+            {
+                NoteId =        entity.NoteId,
+                Title =         entity.Title,
+                Content =       entity.Content,
+                CreatedUtc =    entity.CreatedUtc,
+                ModifiedUtc =   entity.ModifiedUtc
+            };
+        }
+
+        public bool UpdateNote(NoteEdit model)
+        {
+            var entity = _context.Notes.Single(x=>  x.NoteId == model.NoteId && 
+                                                    x.OwnerId == _userId);
+            entity.Title =      model.Title;
+            entity.Content =    model.Content;
+            entity.ModifiedUtc = DateTimeOffset.UtcNow;
+
+            return _context.SaveChanges() == 1;
+        }
+    }
+}
---
+++ ElevenNoteWebMVC/Controllers/NoteController.cs
@@ -0,0 +1,92 @@
+﻿using ElevenNote.Models;
+using ElevenNote.Services;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ElevenNoteWebMVC.Controllers
+{
+    [Authorize]
+    public class NoteController : Controller
+    {
+        private NoteService VerifyUserThenGetService()
+        {
+            var userId = Guid.Parse(User.Identity.GetUserId());
+            var service = new NoteService(userId);
+            return service;
+        }
+
+        public ActionResult Create() => View();
+        public ActionResult Index()
+        {
+            NoteService service = VerifyUserThenGetService();
+            var model = service.GetNotes(); 
+            return View(model);
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(NoteCreate model)
+        {
+            if (!ModelState.IsValid) return View(model);
+            NoteService service = VerifyUserThenGetService();            
+            
+            if (service.CreateNote(model))
+            {
+                TempData["SaveResult"] = "Your note was created.";
+                return RedirectToAction("Index");
+            };
+
+            ModelState.AddModelError("", "Note could not be created.");
+
+            return View(model);
+        }
+
+        public ActionResult Details(int id)
+        {
+            NoteService service = VerifyUserThenGetService();
+            var model = service.GetNoteById(id);
+            return View(model);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            NoteService service = VerifyUserThenGetService();
+            var detail = service.GetNoteById(id);
+            var model = new NoteEdit
+            {
+                NoteId = detail.NoteId,
+                Title = detail.Title,
+                Content = detail.Content
+            };
+            return View(model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, NoteEdit model)
+        {
+            if (!ModelState.IsValid) return View(model);
+
+            if (model.NoteId != id)
+            {
+                ModelState.AddModelError("", "Id Mismatch");
+                return View(model);
+            }
+            var service = VerifyUserThenGetService();
+
+            if (service.UpdateNote(model))
+            {
+                TempData["SaveResult"] = "Your note was updated.";
+                return RedirectToAction("Index");
+            }
+
+            ModelState.AddModelError("", "Your note could not be updated.");
+            return View();
+        }
+    }
+}
